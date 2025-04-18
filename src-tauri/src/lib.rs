9f77c318714f32c63702e62db9ed7a4a4595a101
Tran Van Minh Tri <tvmthtr@gmail.com>
2025-04-18 14:18:17 +0700

    Success to create GUI download truyen

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,14 +1,32 @@
+use futures_util::TryFutureExt;
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+mod metruyenchu;
+
+use metruyenchu::download::download_truyencv;
+use tauri::{AppHandle, Manager};
+
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+async fn start_download(app: AppHandle, base_url: String) -> Result<(), String> {
+    download_truyencv(app, &base_url)
+        .await
+        .map_err(|e| e.to_string())
+}
+use lazy_static::lazy_static;
+use std::sync::atomic::{AtomicBool, Ordering};
+lazy_static! {
+    static ref STOP_FLAG: AtomicBool = AtomicBool::new(false);
+}
+
+#[tauri::command]
+async fn stop_download() {
+    STOP_FLAG.store(true, Ordering::SeqCst);
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![start_download, stop_download])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/App.vue
+++ src/App.vue
@@ -1,160 +1,200 @@
 <script setup lang="ts">
-import { ref } from "vue";
+import { ref, onMounted } from 'vue'
 import { invoke } from "@tauri-apps/api/core";
+import { listen } from '@tauri-apps/api/event'
 
-const greetMsg = ref("");
-const name = ref("");
+const baseUrl = ref('')
+const downloadedChapters = ref<string[]>([])
+const done = ref(false)
+const downloading = ref(false)
+const stop = ref(false)
 
-async function greet() {
-  // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-  greetMsg.value = await invoke("greet", { name: name.value });
+async function stopDownload() {
+    downloading.value = false
+    await invoke('stop_download')
 }
+
+async function startDownload() {
+    downloadedChapters.value = []
+    done.value = false
+    downloading.value = true
+    stop.value = false
+    await invoke('start_download', { baseUrl: baseUrl.value })
+}
+
+onMounted(async () => {
+    await listen<string>('chapter-downloaded', (event) => {
+        downloadedChapters.value.push(event.payload)
+    })
+
+    await listen('download-finished', () => {
+        done.value = true
+    })
+
+    await listen('stop-download', () => {
+        stop.value = true
+    })
+
+    await listen('system-error', (event) => {
+        alert('Lỗi hệ thống: ' + event.payload)
+    })
+})
 </script>
 
 <template>
-  <main class="container">
-    <h1>Welcome to Tauri + Vue</h1>
+    <div style="padding: 10px;">
+        <h1>Downloader</h1>
+        <input v-model="baseUrl" placeholder="Dán link chương 1 ở metruyenchu" class="linkField" />
+        <button @click="startDownload">
+            Bắt đầu tải
+        </button>
 
-    <div class="row">
-      <a href="https://vitejs.dev" target="_blank">
-        <img src="/vite.svg" class="logo vite" alt="Vite logo" />
-      </a>
-      <a href="https://tauri.app" target="_blank">
-        <img src="/tauri.svg" class="logo tauri" alt="Tauri logo" />
-      </a>
-      <a href="https://vuejs.org/" target="_blank">
-        <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-      </a>
+        <button @click="stopDownload" class="bg-red-500 text-white px-4 py-2 mt-2 rounded" v-if="downloading && !done">
+            ⛔ Dừng lại
+        </button>
+
+        <div class="">
+            <h2 class="">Tiến trình:</h2>
+            <ul>
+                <li v-for="(chapter, index) in downloadedChapters" :key="index">
+                    ✅ {{ chapter }}
+                </li>
+            </ul>
+        </div>
+
+        <div v-if="done" class="mt-4 text-green-600 font-bold">🎉 Tải xong rồi!</div>
+        <div v-if="stop" class="mt-4 text-green-600 font-bold">Stop Download</div>
     </div>
-    <p>Click on the Tauri, Vite, and Vue logos to learn more.</p>
-
-    <form class="row" @submit.prevent="greet">
-      <input id="greet-input" v-model="name" placeholder="Enter a name..." />
-      <button type="submit">Greet</button>
-    </form>
-    <p>{{ greetMsg }}</p>
-  </main>
 </template>
 
 <style scoped>
 .logo.vite:hover {
-  filter: drop-shadow(0 0 2em #747bff);
+    filter: drop-shadow(0 0 2em #747bff);
 }
 
 .logo.vue:hover {
-  filter: drop-shadow(0 0 2em #249b73);
+    filter: drop-shadow(0 0 2em #249b73);
 }
-
 </style>
 <style>
 :root {
-  font-family: Inter, Avenir, Helvetica, Arial, sans-serif;
-  font-size: 16px;
-  line-height: 24px;
-  font-weight: 400;
+    font-family: Inter, Avenir, Helvetica, Arial, sans-serif;
+    font-size: 16px;
+    line-height: 24px;
+    font-weight: 400;
 
-  color: #0f0f0f;
-  background-color: #f6f6f6;
+    color: #0f0f0f;
+    background-color: #f6f6f6;
 
-  font-synthesis: none;
-  text-rendering: optimizeLegibility;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-text-size-adjust: 100%;
+    font-synthesis: none;
+    text-rendering: optimizeLegibility;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    -webkit-text-size-adjust: 100%;
 }
 
 .container {
-  margin: 0;
-  padding-top: 10vh;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  text-align: center;
+    margin: 0;
+    padding-top: 10vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    text-align: center;
 }
 
 .logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: 0.75s;
+    height: 6em;
+    padding: 1.5em;
+    will-change: filter;
+    transition: 0.75s;
 }
 
 .logo.tauri:hover {
-  filter: drop-shadow(0 0 2em #24c8db);
+    filter: drop-shadow(0 0 2em #24c8db);
 }
 
 .row {
-  display: flex;
-  justify-content: center;
+    display: flex;
+    justify-content: center;
 }
 
 a {
-  font-weight: 500;
-  color: #646cff;
-  text-decoration: inherit;
+    font-weight: 500;
+    color: #646cff;
+    text-decoration: inherit;
 }
 
 a:hover {
-  color: #535bf2;
+    color: #535bf2;
 }
 
 h1 {
-  text-align: center;
+    text-align: center;
 }
 
 input,
 button {
-  border-radius: 8px;
-  border: 1px solid transparent;
-  padding: 0.6em 1.2em;
-  font-size: 1em;
-  font-weight: 500;
-  font-family: inherit;
-  color: #0f0f0f;
-  background-color: #ffffff;
-  transition: border-color 0.25s;
-  box-shadow: 0 2px 2px rgba(0, 0, 0, 0.2);
+    border-radius: 8px;
+    border: 1px solid transparent;
+    padding: 0.6em 1.2em;
+    font-size: 1em;
+    font-weight: 500;
+    font-family: inherit;
+    color: #0f0f0f;
+    background-color: #ffffff;
+    transition: border-color 0.25s;
+    box-shadow: 0 2px 2px rgba(0, 0, 0, 0.2);
 }
 
 button {
-  cursor: pointer;
+    cursor: pointer;
 }
 
 button:hover {
-  border-color: #396cd8;
+    border-color: #396cd8;
 }
+
 button:active {
-  border-color: #396cd8;
-  background-color: #e8e8e8;
+    border-color: #396cd8;
+    background-color: #e8e8e8;
 }
 
 input,
 button {
-  outline: none;
+    outline: none;
 }
 
 #greet-input {
-  margin-right: 5px;
+    margin-right: 5px;
 }
 
 @media (prefers-color-scheme: dark) {
-  :root {
-    color: #f6f6f6;
-    background-color: #2f2f2f;
-  }
+    :root {
+        color: #f6f6f6;
+        background-color: #2f2f2f;
+    }
 
-  a:hover {
-    color: #24c8db;
-  }
+    a:hover {
+        color: #24c8db;
+    }
 
-  input,
-  button {
-    color: #ffffff;
-    background-color: #0f0f0f98;
-  }
-  button:active {
-    background-color: #0f0f0f69;
-  }
+    input,
+    button {
+        color: #ffffff;
+        background-color: #0f0f0f98;
+    }
+
+    button:active {
+        background-color: #0f0f0f69;
+    }
 }
 
+.linkField {
+    width: 80%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    margin-bottom: 20px;
+}
 </style>
